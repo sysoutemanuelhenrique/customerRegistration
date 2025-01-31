8048a1521e2c90901fe0ed3f31f37bb02ac08887
sysoutemanuelhenrique <emanuel2017.henrique@gmail.com>
2025-01-31 14:44:32 -0300

    feat: continue implementation customer controller

--- src/main/java/com/customerRegistration/aplication/AplicationApplication.java
+++ src/main/java/com/customerRegistration/aplication/AplicationApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AplicationApplication {
 
 
-	public static void main(String[] args) {SpringApplication.run(AplicationApplication.class, args);
+	public static void main(String[] args) {
+		SpringApplication.run(AplicationApplication.class, args);
 
 
+	}
 }
